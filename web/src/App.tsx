3ce0b853c2c59a2c53ffba3e1fd46116944bfcff
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-02-22 09:06:01 +0530

    test

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+
+// import img from './assets/react.svg'
+
+// import HomePage from './components/home/HomePage.jsx'
+import MyPostion from './components/MyPosition.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <MyPostion/>
+    </div>
+  )
+}
+
+export default App
---
+++ web/src/components/home/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>Home dwa</div>
+  )
+}
+
+export default HomePage
